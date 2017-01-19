ab884bb569db4c7a2f739d3e30258931ae9cc034
Juan Manuel Lomas <juanmanuellf@gmail.com>
2017-01-19 12:05:13 +0100

    Agregada funcionalidad basica web al war

---
+++ supermercado-war/src/main/java/web/ArticuloController.java
@@ -0,0 +1,67 @@
+package web;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import business.business.GestionArticulos;
+import common.domain.Articulo;
+import common.utils.ArticuloYaExisteException;
+
+
+@Named
+@SessionScoped
+public class ArticuloController implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Articulo articulo = new Articulo();
+	private List<Articulo> articulos;
+	
+	// Si lo hiciesemos con un EJB de la capa de negocio
+	@EJB
+	private GestionArticulos articuloEJB;
+	
+	public ArticuloController() {		
+	}
+
+	public String altaArticulo() {
+		
+		 FacesContext context = FacesContext.getCurrentInstance();  
+		
+		try{
+			
+			articulo = articuloEJB.altaArticulo(articulo);
+			articulos = articuloEJB.verArticulos();
+			return "listaArticulos.xhtml";
+			
+		}catch(ArticuloYaExisteException e){
+			context.addMessage(null, new FacesMessage("Error, el articulo ya existe"));	        
+	        return null;
+		}
+		
+	}
+	
+	
+	public Articulo getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+
+	public List<Articulo> getArticulos() {
+		return articulos;
+	}
+
+	public void setArticulos(List<Articulo> articulos) {
+		this.articulos = articulos;
+	}
+
+}
